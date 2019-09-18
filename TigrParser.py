76ce93e84ecd43174e5a3938370c32a0a6e1b5dd
Kelsey <kelzling@gmail.com>
2019-09-19 11:17:31 +1200

    extracted _parse_line() method

--- TigrParser.py
+++ TigrParser.py
@@ -27,6 +27,22 @@ class TigrParser(AbstractParser):
         # readonly
         return self.__output_log
 
+    def _parse_line(self):
+        match = re.findall(self.regex_pattern, self.current_line)
+        if match:
+            groups = match[0]
+            self.command = groups[0].upper()
+            if groups[1]:
+                self.data = int(round(float(groups[1])))
+                """ Parser accepts decimals but silently rounds them in the background - all numbers passed are
+                stored as integers"""
+            else:
+                self.data = None
+        else:
+            # Raises SyntaxError to indicate that the line line_number didn't match the required pattern
+            raise SyntaxError(
+                f"line number {self.current_line_number} contains invalid syntax: \n\t{self.current_line}")
+
     def parse(self, raw_source):
         if type(raw_source) == str:  # defensively handles edge case where a single command was passed as a string
             raw_source = [raw_source]
@@ -37,46 +53,34 @@ class TigrParser(AbstractParser):
             if not trimmed_line:
                 continue
             self.current_line = trimmed_line
-            match = re.findall(self.regex_pattern, self.current_line)
-            if match:
-                groups = match[0]
-                self.command = groups[0].upper()
-                if groups[1]:
-                    self.data = int(round(float(groups[1])))
-                    """ Parser accepts decimals but silently rounds them in the background - all numbers passed are
-                    stored as integers"""
-                else:
-                    self.data = None
+            self._parse_line()
 
-                command_info = self.language_commands.get(self.command)
-                if command_info:
-                    args = []
-                    if len(command_info) > 1:
-                        args.append(*command_info[1])
-                    if self.data:
-                        args.append(self.data)
+            command_info = self.language_commands.get(self.command)
+            if command_info:
+                args = []
+                if len(command_info) > 1:
+                    args.append(*command_info[1])
+                if self.data:
+                    args.append(self.data)
 
-                    # explodes the created args array into the function that is being called
-                    # if there is nothing in the array, nothing will be passed! Nice and fancy.
-                    try:
-                        output = self.drawer.__getattribute__(command_info[0])(*args)
-                        self.__output_log.append(output)
-                    except AttributeError as e:
-                        raise SyntaxError(
-                            f'Command {self.command} Not recognized by drawer - Command reference mismatch detected')
-                    except Exception as e:  # intercept error thrown that wasn't caught and appending the line number
-                        # that caused it
-                        args = e.args
-                        if args:
-                            arg0 = args[0]
-                        else:
-                            arg0 = str()
-                        arg0 += f' at source line {self.current_line_number}'
-                        e.args = (arg0, *args[1:])
-                        raise
+                # explodes the created args array into the function that is being called
+                # if there is nothing in the array, nothing will be passed! Nice and fancy.
+                try:
+                    output = self.drawer.__getattribute__(command_info[0])(*args)
+                    self.__output_log.append(output)
+                except AttributeError as e:
+                    raise SyntaxError(
+                        f'Command {self.command} Not recognized by drawer - Command reference mismatch detected')
+                except Exception as e:  # intercept error thrown that wasn't caught and appending the line number
+                    # that caused it
+                    args = e.args
+                    if args:
+                        arg0 = args[0]
+                    else:
+                        arg0 = str()
+                    arg0 += f' at source line {self.current_line_number}'
+                    e.args = (arg0, *args[1:])
+                    raise
 
-                else:
-                    raise SyntaxError(f"Command {self.command} on line {self.current_line_number} not recognized")
             else:
-                # Raises SyntaxError to indicate that the line line_number didn't match the required pattern
-                raise SyntaxError(f"line number {self.current_line_number} contains invalid syntax: \n\t{self.current_line}")
+                raise SyntaxError(f"Command {self.command} on line {self.current_line_number} not recognized")
